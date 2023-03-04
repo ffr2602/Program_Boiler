56fb2e0f75f5fa63a51c4bc9ff9df879b08b3491
Fahmi Fathur Rohman <100126219+ffr2602@users.noreply.github.com>
2023-03-04 19:01:47 +0700

    Update Program_Boiler.ino

--- Program_Boiler.ino
+++ Program_Boiler.ino
@@ -2,6 +2,11 @@
 #include <AccelStepper.h>
 #include <max6675.h>
 
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+//--> Set Pin
+//----------------------------------------------------------------------------------------------------------------
+
 //--> Motor STEPPER pin
 #define pin_STEP 3
 #define pin_DIR 2
@@ -30,15 +35,30 @@
 #define pin_SL01_IN 34
 #define pin_PEMATIK 50
 
+//----------------------------------------------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
 //--> Kelompok Data
-bool data_select = false;
-const int speed_set[2] = { 100, 1000 };
-const int data_open[2] = { 0, 50 };
+//----------------------------------------------------------------------------------------------------------------
+
+const int speed_set[2] = { 500, 1000 };
+const int data_open[3] = { 0, 80, 100 };
+
+const float setpoint_data_tekanan = 0.50;
+
+bool data_select[2] = { false, false };
+
 float data_suhu = 0;
 float data_tekanan = 0;
+
 int counter = 0;
+
 unsigned long time = 0;
+unsigned long time_1 = 0;
+unsigned long data_count = 0;
+
+//----------------------------------------------------------------------------------------------------------------
 
 AccelStepper stepper(AccelStepper::DRIVER, pin_STEP, pin_DIR);
 MAX6675 sensor_suhu(pin_SCK, pin_CS, pin_S0);
@@ -61,11 +81,15 @@ void setup() {
   pinMode(pin_PEMATIK, OUTPUT);
   pinMode(pin_BUZZ, OUTPUT);
 
+  //--> Set OFF All Relay
+  digitalWrite(pin_PUMP, HIGH);
+  digitalWrite(pin_SL01_IN, HIGH);
+  digitalWrite(pin_SL02_OUT, HIGH);
+  digitalWrite(pin_PEMATIK, HIGH);
+
   //--> Set Motor STEPPER
   stepper.setMaxSpeed(speed_set[1]);
   stepper.setSpeed(speed_set[0]);
-
-  relay(1, 1, 1, 1);
 
   //-- Setup LCD
   lcd.init();
@@ -84,80 +108,108 @@ void loop() {
     lcd.print("Status:");
     lcd.setCursor(0, 1);
     lcd.print("READY...");
-    delay(2000);
+    delay(3000);
     lcd.clear();
   } else if (counter > 0) {
     lcd.setCursor(0, 0);
     lcd.print("Status:");
     lcd.setCursor(0, 1);
     lcd.print("Isi Ulang");
-    delay(2000);
+    delay(3000);
     lcd.clear();
   }
 
   //--> Pompa & Selonoid Air Masuk
   while (true) {
+    //--> jika Air penuh
     if (digitalRead(batas_atas) == LOW && digitalRead(batas_bawah) == LOW) {
-      data_select = true;
+      data_select[0] = true;
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Status:");
       lcd.setCursor(0, 1);
       lcd.print("Air Penuh");
-      relay(1, 0, 0, 1);
+      digitalWrite(pin_PUMP, HIGH);
       delay(3000);
-      relay(1, 1, 0, 1);
+      digitalWrite(pin_SL01_IN, HIGH);
       delay(3000);
-      relay(1, 1, 1, 1);
+      digitalWrite(pin_SL02_OUT, HIGH);
+      delay(2000);
       lcd.clear();
       break;
     }
+    //--> Jika Air belum penuh
     lcd.setCursor(0, 0);
     lcd.print("Status:");
     lcd.setCursor(0, 1);
     lcd.print("Pengisian Air");
-    if (data_select == false) {
-      relay(1, 0, 1, 1);
+    if (data_select[0] == false) {
+      digitalWrite(pin_SL02_OUT, LOW);
       delay(2000);
-      relay(1, 0, 0, 1);
+      digitalWrite(pin_SL01_IN, LOW);
       delay(2000);
-      relay(0, 0, 0, 1);
+      digitalWrite(pin_PUMP, LOW);
       delay(2000);
     }
-    Serial.println("batas bawah: " + String(batas_bawah) + "|| batas atas: " + String(batas_atas));
   }
 
   //--> Kirim Data & Baca Suhu & Baca Tekanan & Kendali Output Selonoid Gas
   while (true) {
-    if (millis() - time > 500) {
+    //--> Baca suhu dan tekanan
+    if (millis() - time >= 2000) {
       baca_suhu_dan_tekanan();
       time = millis();
     }
-    digitalWrite(pin_PEMATIK, LOW);
-    if (digitalRead(pin_SENSOR_API) == HIGH) {
-      digitalWrite(pin_BUZZ, HIGH);
+    //--> Menghidupkan Pematik untuk menyalakan Api
+    while (data_select[1] == false) {
       digitalWrite(pin_PEMATIK, LOW);
-      motor_stepper_move(data_open[1]);
-    } else if (digitalRead(pin_SENSOR_API) == LOW) {
+      digitalWrite(pin_BUZZ, HIGH);
+      motor_stepper_move(data_open[0]);
+      if (digitalRead(pin_SENSOR_API == LOW)) {
+        digitalWrite(pin_PEMATIK, HIGH);
+        data_select[1] = true;
+        break;
+      }
+    }
+    //--> Mematikan Pematik ketika Api menyala
+    if (digitalRead(pin_SENSOR_API) == HIGH) {
+      data_select[1] = false;
+    }
+    //--> Mengatur OUTPUT GAS LPG untuk mengatur Besar Kecil Api
+    if (digitalRead(pin_SENSOR_API) == LOW) {
       digitalWrite(pin_BUZZ, LOW);
-      digitalWrite(pin_PEMATIK, HIGH);
-      if (data_suhu >= 100) {
-        motor_stepper_move(80);
-      } else if (data_suhu < 100) {
+      if (data_tekanan >= setpoint_data_tekanan) {
+        motor_stepper_move(data_open[1]);
+      } else if (data_tekanan < setpoint_data_tekanan) {
         motor_stepper_move(data_open[0]);
       }
     }
-    if (data_tekanan > 4.00) {
+    //--> Mengatur OUTPUT GAS UAP Air
+    if (data_tekanan >= setpoint_data_tekanan) {
       digitalWrite(pin_SL02_OUT, LOW);
-    } else if (digitalRead(batas_bawah == HIGH) && digitalRead(batas_atas) == HIGH) {
-      motor_stepper_move(100);
+    } else if (data_tekanan < setpoint_data_tekanan) {
+      digitalWrite(pin_SL02_OUT, HIGH);
+    }
+    //--> Mengatur Ulang Proses jika Air Habis
+    if (digitalRead(batas_bawah == HIGH) && digitalRead(batas_atas) == HIGH) {
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Status:");
       lcd.setCursor(0, 1);
       lcd.print("Air Habis");
       digitalWrite(pin_PEMATIK, HIGH);
+      delay(2000);
       digitalWrite(pin_SL02_OUT, HIGH);
+      delay(2000);
+      data_select[0] = false;
+      while (true) {
+        motor_stepper_move(data_open[2]);
+        data_count += 1;
+        if (data_count == 10000) {
+          break;
+        }
+      }
+      data_count = 0;
       counter += 1;
       lcd.clear();
       break;
@@ -165,29 +217,26 @@ void loop() {
   }
 }
 
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+//--> Sub Program
+//----------------------------------------------------------------------------------------------------------------
+
 void motor_stepper_move(float posisi) {
-  volatile int step_posisi = map(posisi, 100.00, 0.00, 0, 800) / 4;
+  int step_posisi = map(posisi, 100.00, 0.00, 0, 800) / 4;
   stepper.moveTo(step_posisi);
   stepper.setSpeed(speed_set[0]);
   stepper.runSpeedToPosition();
 }
 
 void baca_suhu_dan_tekanan() {
-  lcd.setCursor(0, 0);
-  lcd.print("Suhu: " + String(data_suhu) + char(223) + "C");
-  lcd.setCursor(0, 1);
-  lcd.print("Press: " + String(data_tekanan) + " Bar");
   int ADC_in = analogRead(pin_ADC);
   float voltage = 5.00 * ADC_in / 1023.00;
   float pascal = (3.0 * ((float)voltage - 0.47)) * 1000000.0;
-  float data_temp = pascal / 10e5;
-  data_tekanan = map(data_temp, 0.00, 12.00, 0.00, 4.00);
+  data_tekanan = pascal / 10e5;
   data_suhu = sensor_suhu.readCelsius();
-}
-
-void relay(int a, int b, int c, int d) {
-  digitalWrite(pin_PUMP, a);
-  digitalWrite(pin_SL01_IN, b);
-  digitalWrite(pin_SL02_OUT, c);
-  digitalWrite(pin_PEMATIK, d);
+  lcd.setCursor(0, 0);
+  lcd.print("Suhu: " + String(data_suhu) + char(223) + "C ");
+  lcd.setCursor(0, 1);
+  lcd.print("Press: " + String(data_tekanan) + " Bar ");
 }
